48eb8379cc140bf04dbd949ce1d2fc26f93e00ef
Jmour-dh <jmour-dhiaeddine@hotmail.com>
2025-04-09 20:04:08 +0200

    init backend

---
+++ Backend_PMT/src/main/java/com/PMT/Backend_PMT/BackendPmtApplication.java
@@ -0,0 +1,13 @@
+package com.PMT.Backend_PMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPmtApplication.class, args);
+	}
+
+}
---
+++ Backend_PMT/src/test/java/com/PMT/Backend_PMT/BackendPmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PMT.Backend_PMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
