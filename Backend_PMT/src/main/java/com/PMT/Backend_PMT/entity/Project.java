c43937dee207a52924464e2c0f7ac6cb504963ea
Jmour-dh <jmour-dhiaeddine@hotmail.com>
2025-04-10 16:49:45 +0200

    create entity project

--- Backend_PMT/src/main/java/com/PMT/Backend_PMT/entity/Project.java
+++ Backend_PMT/src/main/java/com/PMT/Backend_PMT/entity/Project.java
@@ -1,4 +1,21 @@
 package com.PMT.Backend_PMT.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "projects")
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private LocalDate startDate;
 }
