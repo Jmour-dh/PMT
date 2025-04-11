b2f5c9aea426302a7514acd0c471f8f8c9b0dc5b
Jmour-dh <jmour-dhiaeddine@hotmail.com>
2025-04-11 10:32:32 +0200

    exception

---
+++ Backend_PMT/src/main/java/com/PMT/Backend_PMT/service/UserService.java
@@ -0,0 +1,14 @@
+package com.PMT.Backend_PMT.service;
+
+import com.PMT.Backend_PMT.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+}
