2c4de6f76a5dc24fd4d1f72f354eec9472fa4f97
Jmour-dh <jmour-dhiaeddine@hotmail.com>
2025-04-14 22:20:31 +0200

    signup

--- Backend_PMT/src/main/java/com/PMT/Backend_PMT/controller/AuthController.java
+++ Backend_PMT/src/main/java/com/PMT/Backend_PMT/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.PMT.Backend_PMT.controller;
 
 import com.PMT.Backend_PMT.dto.AuthDto;
+import com.PMT.Backend_PMT.dto.UserDto;
+import com.PMT.Backend_PMT.entity.User;
 import com.PMT.Backend_PMT.service.AuthService;
+import com.PMT.Backend_PMT.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,10 +17,17 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody AuthDto loginRequest) {
         String token = authService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
         return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@Valid @RequestBody UserDto userDto) {
+        User createdUser = userService.createUser(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 }
